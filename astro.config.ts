03421db4cb2f859d18eab49d582207a68f108ebd
sawangpong muadphet <sawangpong@itbakery.net>
2025-08-07 17:27:07 +0700

    Edit astro.config.ts

--- astro.config.ts
+++ astro.config.ts
@@ -23,7 +23,7 @@ const whenExternalScripts = (items: (() => AstroIntegration) | (() => AstroInteg
 
 export default defineConfig({
   output: 'static',
-  site: "https://itbakery.github.io/itbakery.net/",
+  site: "https://itbakery.github.io/",
   base: "itbakery.net",
   integrations: [
     tailwind({
